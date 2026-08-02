ffa60187e21776cfc97847b5c69d70740c885ed3
SANTHOSH P <santhosh11042007@gmail.com>
2026-08-02 09:04:50 +0530

    Create tests/test_loader.py

---
+++ tests/test_loader.py
@@ -0,0 +1,9 @@
+from code.data_loader import load_all
+
+
+def test_loader():
+    data = load_all()
+
+    assert len(data.messages) > 0
+    assert len(data.users) > 0
+    assert len(data.sample_messages) > 0
